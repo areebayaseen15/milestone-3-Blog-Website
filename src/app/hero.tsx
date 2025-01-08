718203c9585ea4106ae2d671f389c4d13ec44988
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-01-08 15:35:09 +0500

    Update hero.tsx

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -3,7 +3,7 @@ export default function HeroSection() {
     return (
         <div>
       <section className="mt-28  relative h-[900px] bg-cover bg-center " style={{backgroundImage: "url('/Assets/bg2.png')"} }>
-        <div className="absolute  left-20 top-28 md:top-0 bg-[#8d5a3a] p-10 rounded-md">
+        <div className="absolute  left-16 top-28 md:top-0 bg-[#8d5a3a] p-10 rounded-md">
           <img src="/Assets/profile.avif" alt="Profile" className="w-24 h-24 rounded-full mx-auto"/>
           <h2 className="text-4xl text-white mt-5">Welcome!</h2>
           <p className="text-white">Join my journey</p>
